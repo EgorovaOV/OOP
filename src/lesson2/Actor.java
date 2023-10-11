b43f4815b9a823e8208a030fd712a6b1bdbaba9f
Olga <ove2501@mail.ru>
2023-10-11 21:16:05 +0500

    all

---
+++ src/lesson2/Actor.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public abstract class Actor {
+    protected String name;
+    protected boolean isMakeOrder;
+    protected boolean isTakeOrder;
+
+    abstract String getName();
+}
